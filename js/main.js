73db259d235ebd624331baead065bf8cdab5517b
Yefferson <steelchavez53@gmail.com>
2025-05-21 13:05:42 -0500

    All work

--- js/main.js
+++ js/main.js
@@ -41,10 +41,11 @@
 
 
     // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+   $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+});
+
 
 
     // Date and time picker
