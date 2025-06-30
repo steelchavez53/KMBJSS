72b1a33e8855e77a7a759107a29e2aeec800ed74
Yefferson <steelchavez53@gmail.com>
2025-06-30 12:25:33 -0500

    all modified

---
+++ css/public_html/PHPform-smtp-1.0/smtp.php
@@ -0,0 +1,47 @@
+<?php
+
+$Nombre = $_POST['Nombre'];
+$Correo = $_POST['Correo'];
+$Mensaje = $_POST['Mensaje'];
+
+if ($Nombre=='' || $Correo=='' || $Mensaje==''){
+
+echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
+
+}else{
+
+    require("src/PHPMailer.php");
+    require("src/SMTP.php");
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->SMTPDebug = 2;
+    $mail->setFrom('yefferson@kmbjironworks.com'); // Dirección de la que llegan los mensajes. Debe ser igual a la configurada para envíos.
+    $mail->FromName = $Nombre;
+    $mail->AddAddress("sales@kmbjironworks.com"); // Dirección a la que llegaran los mensajes.
+
+    // Aquí van los datos que apareceran en el correo que reciba
+
+    $mail->WordWrap = 50;
+    $mail->IsHTML(true);
+    $mail->Subject  =  "Contacto";
+    $mail->Body =  "Nombre: $Nombre \n<br />".
+    "Email: $Correo \n<br />".
+    "Mensaje: $Mensaje \n<br />";
+
+    // Datos del servidor SMTP
+//
+    $mail->IsSMTP();
+    $mail->Host = "mail.kmbjironworks.com";  // Servidor de Salida.
+    $mail->SMTPAuth = true; // Autenticación SMTP activada
+    $mail->SMTPSecure = 'tls'; // Seguridad SMTP
+    $mail->Port = 587; // Puerto de autenticación
+    $mail->Username = "yoselin@kmbjironworks.com";  // Correo Electrónico que envía correo
+    $mail->Password = "S[UYu[]Ysb(v"; // Contraseña de cuenta de correo
+
+    if ($mail->Send())
+    echo "<script>alert('Formulario Enviado');location.href ='enviado.html';</script>"; // Respuesta en caso de ser correcto el envío, redireccionará a enviado.html
+    else
+    echo "<script>alert('Error al enviar el formulario');location.href ='javascript:history.back()';</script>"; // Respuesta en caso de ser incorrecto el envío, regresará al formulario
+
+}
+?>
---
+++ css/public_html/procesar_formulario.php
@@ -0,0 +1,103 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Clave secreta de reCAPTCHA
+    $recaptcha_secret_key = "6LchE_8pAAAAAHd0NNv-qaLeac5dqops4J_5_e-4";
+    
+    // Respuesta reCAPTCHA enviada desde el formulario
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+
+    // Verificación de reCAPTCHA con cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+        'secret' => $recaptcha_secret_key,
+        'response' => $recaptcha_response
+    )));
+    $recaptcha_result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo json_encode(["success" => false, "error" => 'Error connecting to Google reCAPTCHA: ' . curl_error($ch)]);
+        curl_close($ch);
+        exit();
+    }
+    curl_close($ch);
+    $recaptcha_result = json_decode($recaptcha_result, true);
+
+    if (!$recaptcha_result['success']) {
+        echo json_encode(["success" => false, "error" => "Failed to verify reCAPTCHA."]);
+        exit();
+    }
+
+    // Datos del formulario
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $company = $_POST['company'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address1 = $_POST['address1'];
+    $city = $_POST['city'];
+    $stateprovince = $_POST['stateprovince'];
+    $postal = $_POST['postal'];
+    $message = $_POST['message'];
+
+    // Crear una instancia de PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Configuración del servidor de correo
+        $mail->isSMTP();
+        $mail->Host = 'mail.kmbjironworks.com'; 
+        $mail->SMTPAuth = true;
+        $mail->Username = 'sales@kmbjironworks.com'; 
+        $mail->Password = 'Fo+Os&e6Op*E'; 
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Remitente y destinatarios
+        $mail->setFrom('yefferson@kmbjironworks.com', 'kmbjironworks');
+        $mail->addAddress('sales@kmbjironworks.com');
+         $mail->addCC($email); 
+        $mail->addCC('estimatingdepartment@kmbj.info');
+        $mail->addCC('ulises@kmbj.info');
+        $mail->addCC('office@kmbj.info');
+   
+
+        // Adjuntar el archivo si se ha subido
+        if (isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK) {
+            $file_name = $_FILES['file']['name'];
+            $file_tmp = $_FILES['file']['tmp_name'];
+            $mail->addAttachment($file_tmp, $file_name);
+        }
+
+        // Contenido del correo
+        $mail->isHTML(false);
+        $mail->Subject = 'New contact form';
+        $mail->Body = "Name: $firstname\n" .
+                      "Lastname:  $lastname\n" .
+                      "Company: $company\n" .
+                      "E-mail: $email\n" .
+                      "Phone: $phone\n" .
+                      "Address: $address1\n" .
+                      "City: $city\n" .
+                      "State: $stateprovince\n" .
+                      "Postal Code: $postal\n" .
+                      "Message: $message\n";
+
+        // Enviar el correo
+        $mail->send();
+        echo json_encode(["success" => true]);
+    } catch (Exception $e) {
+        echo json_encode(["success" => false, "error" => "Failed to send email. Mailer Error: {$mail->ErrorInfo}"]);
+    }
+}
+?>
+
+
